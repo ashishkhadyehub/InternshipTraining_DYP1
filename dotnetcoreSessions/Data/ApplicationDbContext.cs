1698638c9473a2f549d32910b41cec29fd924197
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-03-14 12:46:25 +0530

    dotnet core session 3

---
+++ dotnetcoreSessions/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using dotnetcoreSessions.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetcoreSessions.Data
+{
+    //database
+    public class ApplicationDbContext : DbContext
+    {
+        //ctor
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+       
+    }
+}
